f78d48a36a07fb45610a319921f1040a177dfc76
Joe Gultekin <joe@e-pod.com.au>
2021-10-07 00:59:27 +0200

    Version 4 - added env-init Bash script - take 7

--- test-scenario/env-init.sh
+++ test-scenario/env-init.sh
@@ -1,3 +1,6 @@
 sudo apt update -y
-sudo apt upgrade -y
-sudo apt install -y podman
+source /etc/os-release
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
+wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_${VERSION_ID}/Release.key -O- | sudo apt-key add -
+sudo apt update -qq
+sudo apt-get -qq --yes install podman
